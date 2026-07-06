9f4f6a188af85a1cea70ddc7c3362def725a8d13
hduelme <hduelme@gmx.de>
2026-07-06 16:04:30 +0200

    continue

--- src/main/java/org/mapstruct/intellij/inspection/SubclassMappingDoubleSourceSubclassInspection.java
+++ src/main/java/org/mapstruct/intellij/inspection/SubclassMappingDoubleSourceSubclassInspection.java
@@ -5,7 +5,35 @@
  */
 package org.mapstruct.intellij.inspection;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.mapstruct.intellij.util.MapstructAnnotationUtils.extractSubclassMappingAnnotations;
+import static org.mapstruct.intellij.util.MapstructAnnotationUtils.findAllDefinedSubclassMappingAnnotations;
+import static org.mapstruct.intellij.util.MapstructAnnotationUtils.isSubclassMappingPsiAnnotation;
+import static org.mapstruct.intellij.util.MapstructAnnotationUtils.isSubclassMappingsPsiAnnotation;
+import static org.mapstruct.intellij.util.TargetUtils.getTargetType;
+
+import com.intellij.codeInsight.intention.QuickFixFactory;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
+import com.intellij.codeInspection.LocalQuickFixOnPsiElement;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.codeInspection.util.IntentionFamilyName;
+import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.openapi.editor.CaretState;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.editor.ScrollType;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.JavaElementVisitor;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiAnnotationMemberValue;
@@ -13,25 +41,13 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiClassObjectAccessExpression;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiType;
+import com.intellij.psi.impl.source.tree.java.PsiAnnotationImpl;
 import org.jetbrains.annotations.NotNull;
+import org.mapstruct.intellij.MapStructBundle;
 import org.mapstruct.intellij.util.MapstructUtil;
-import org.mapstruct.intellij.util.TargetUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static org.mapstruct.intellij.util.MapstructAnnotationUtils.extractSubclassMappingAnnotations;
-import static org.mapstruct.intellij.util.MapstructAnnotationUtils.findAllDefinedSubclassMappingAnnotations;
-import static org.mapstruct.intellij.util.MapstructAnnotationUtils.findSubclassMetaAnnotations;
-import static org.mapstruct.intellij.util.MapstructAnnotationUtils.isSubclassMappingPsiAnnotation;
-import static org.mapstruct.intellij.util.MapstructAnnotationUtils.isSubclassMappingsPsiAnnotation;
-import static org.mapstruct.intellij.util.TargetUtils.getTargetType;
 
 public class SubclassMappingDoubleSourceSubclassInspection extends InspectionBase {
     @NotNull
@@ -70,11 +86,13 @@ public class SubclassMappingDoubleSourceSubclassInspection extends InspectionBas
                     handleAnnotationWithMappingAnnotation(psiAnnotation, problemMap);
                 }
             }
+            QuickFixFactory quickFixFactory = QuickFixFactory.getInstance();
             for ( Map.Entry<PsiType, List<PsiElement>> problem : problemMap.entrySet() ) {
                 List<PsiElement> problemElements = problem.getValue();
-                if (problemElements.size() > 1) {
+                if ( problemElements.size() > 1 ) {
                     for (PsiElement problemElement : problemElements) {
-                        holder.registerProblem( problemElement, problem.getKey() + " Double" );
+                        LocalQuickFix[] quickFixes = getLocalQuickFixes( problemElement, quickFixFactory );
+                        holder.registerProblem( problemElement, problem.getKey() + " Double" , quickFixes);
                     }
                 }
             }
@@ -104,6 +122,77 @@ public class SubclassMappingDoubleSourceSubclassInspection extends InspectionBas
                         PsiType sourceType = sourceClass.getOperand().getType();
                         problemMap.computeIfAbsent( sourceType, k -> new ArrayList<>() ).add( psiAnnotation );
                     });
+        }
+
+        private static @NotNull  LocalQuickFix[] getLocalQuickFixes(PsiElement problemElement,
+                                                                    QuickFixFactory quickFixFactory) {
+            List<LocalQuickFix> quickFixes = new ArrayList<>(2);
+            if ( problemElement instanceof PsiAnnotation ) {
+                quickFixes.add( getDeleteFix( problemElement, quickFixFactory ) );
+            }
+            else if ( problemElement instanceof PsiAnnotationMemberValue problemPsiAnnotationMemberValue ) {
+                Optional.ofNullable( problemElement.getParent() ).map( PsiElement::getParent )
+                        .map( PsiElement::getParent ).filter( PsiAnnotation.class::isInstance )
+                        .ifPresent( annotation -> quickFixes.add(
+                                getDeleteFix( annotation, quickFixFactory ) ) );
+                quickFixes.add( new ChangeTargetQuickFix( problemPsiAnnotationMemberValue ) );
+            }
+            return quickFixes.toArray( new LocalQuickFix[]{} );
+        }
+
+        private static @NotNull LocalQuickFixAndIntentionActionOnPsiElement getDeleteFix(
+                @NotNull PsiElement problemElement, @NotNull QuickFixFactory quickFixFactory) {
+
+            String annotationName = PsiAnnotationImpl.getAnnotationShortName( problemElement.getText() );
+            return quickFixFactory.createDeleteFix( problemElement,
+                    MapStructBundle.message( "intention.remove.annotation", annotationName ) );
+        }
+
+        private static class ChangeTargetQuickFix extends LocalQuickFixOnPsiElement {
+
+            private final String myText;
+            private final String myFamilyName;
+
+            private ChangeTargetQuickFix(@NotNull PsiAnnotationMemberValue element) {
+                super( element );
+                myText = MapStructBundle.message( "intention.change.target.property" );
+                myFamilyName = MapStructBundle.message( "inspection.target.property.mapped.more.than.once",
+                        element.getText() );
+            }
+
+            @Override
+            public @IntentionName @NotNull String getText() {
+                return myText;
+            }
+
+            @Override
+            public void invoke(@NotNull Project project, @NotNull PsiFile psiFile, @NotNull PsiElement psiElement,
+                               @NotNull PsiElement psiElement1) {
+                FileEditor selectedEditor = FileEditorManager.getInstance( project ).getSelectedEditor();
+                if ( selectedEditor instanceof TextEditor textEditor ) {
+                    Editor editor = textEditor.getEditor();
+
+                    TextRange textRange = ((PsiClassObjectAccessExpression) psiElement).getOperand().getTextRange();
+
+                    editor.getCaretModel().moveToOffset( textRange.getStartOffset() );
+                    LogicalPosition startPosition = editor.getCaretModel().getLogicalPosition();
+                    editor.getCaretModel().moveToOffset( textRange.getEndOffset() );
+                    editor.getCaretModel().setCaretsAndSelections(
+                            Collections.singletonList( new CaretState(startPosition, startPosition,
+                                    editor.getCaretModel().getLogicalPosition() ) ) );
+                    editor.getScrollingModel().scrollToCaret( ScrollType.MAKE_VISIBLE );
+                }
+            }
+
+            @Override
+            public @IntentionFamilyName @NotNull String getFamilyName() {
+                return myFamilyName;
+            }
+
+            @Override
+            public boolean availableInBatchMode() {
+                return false;
+            }
         }
 
     }
